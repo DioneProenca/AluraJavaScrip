e155f0fd3ee40eb01f1a6567460cb0b0278a7422
DioneProenca <98290624+DioneProenca@users.noreply.github.com>
2022-05-09 23:27:11 -0300

    Create 8-loops.js

---
+++ 8-loops.js
@@ -0,0 +1,34 @@
+console.log(`\n Trabanlhando com Condicionais`);
+
+
+const listaDeDestinos = new Array(
+    `Salvador`,
+    `São Paulo`,
+    `Rio de Janeiro`,
+
+);
+
+
+const idadeComprador = 17;
+const estaAcompanhada = true;
+let  temPassagemComprada = false;
+const destino = "Salvador";
+
+console.log("\n Destinos possíveis:");
+console.log(listaDeDestinos);
+
+
+const podeComprar = idadeComprador >= 18 || estaAcompanhada == true;
+    
+let contador = 0;
+while(contador < 3){
+    if(listaDeDestinos[contador] == destino){
+        console.log("Destino existe");
+    }else{
+        console.log("Destino não existe");
+    }
+
+
+    contador += 1;
+}
+
